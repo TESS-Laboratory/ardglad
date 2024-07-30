a07f8b4b3c907f7b44df07db1858ff3dc3c97bba
Hugh Graham <hugh.a.graham@gmail.com>
2024-07-30 15:28:43 +0100

    refactor to use one class only - avoid spatrasters on return

--- R/ard-glad-download.R
+++ R/ard-glad-download.R
@@ -2,8 +2,8 @@
 #' @param glad_urls a list of URLs to download from `ard_glad_urls`
 #' @param dir a character string of the directory to save the files
 #' @param prefix a character string to prefix the subdirectory/filenames
-#' @param return_filename a logical value indicating whether to return the
-#' filename or the SpatRaster object
+#' @param quiet a logical to suppress messages
+#' @param ... additional arguments not used
 #' @return a list of SpatRaster objects or filenames if `return_filename` is
 #' TRUE
 #' @export
@@ -13,11 +13,13 @@
 ard_glad_download <- function(
     glad_urls, dir,
     prefix = dir,
-    return_filename = FALSE) {
+    quiet = TRUE, ...) {
   UseMethod("ard_glad_download")
 }
 
 #' method for default sources - not supported
+#' @rdname ard-glad-download
+#' @family GLAD ARD download
 #' @export
 ard_glad_download.default <- function(glad_urls, ...) {
   cli::cli_abort(
@@ -29,6 +31,8 @@ ard_glad_download.default <- function(glad_urls, ...) {
 }
 
 #' method for AWS sources - not supported
+#' @rdname ard-glad-download
+#' @family GLAD ARD download
 #' @export
 ard_glad_download.ard_glad_urls_aws <- function(glad_urls, ...) {
   cli::cli_abort(
@@ -40,34 +44,29 @@ ard_glad_download.ard_glad_urls_aws <- function(glad_urls, ...) {
 }
 
 #' method for UMD sources
+#' @rdname ard-glad-download
+#' @family GLAD ARD download
 #' @export
 ard_glad_download.ard_glad_urls_umd <- function(
-    glad_urls, dir, prefix = dir, return_filename = FALSE) {
+    glad_urls, dir, prefix = dir, quiet = TRUE, ...) {
+  assertthat::assert_that(is.character(dir))
+  assertthat::assert_that(is.character(prefix))
+  assertthat::assert_that(is.logical(quiet))
+
   glad <- future.apply::future_mapply(
     \(x, y) {
-      ard_glad_download_ts(x, y, dir, prefix, return_filename)
+      ard_glad_download_ts(x, y, dir, prefix, quiet, return_filename = TRUE)
     },
     as.list(glad_urls),
-    as.list(names(glad_urls))
+    as.list(names(glad_urls)),
+    future.seed = TRUE
   )
 
-  newclass <- ifelse(return_filename, "ard_glad_src", "ard_glad")
-  class(glad) <- c(newclass, class(glad))
-
-  return(glad)
-}
-
-#' generic to coerce ard_glad_urls objects to a list
-#' @export
-#' @noRd
-#' @keywords internal
-as.list.ard_glad <- function(x, ...) {
-  class(x) <- "list"
-  return(x)
+  return(build_ard_glad(glad))
 }
 
 
-ard_glad_download_ts <- function(x, name, dir, prefix, return_filename) {
+ard_glad_download_ts <- function(x, name, dir, prefix, quiet, return_filename) {
   name_split <- strsplit(x, "/")
 
   mk_name <- paste(
@@ -92,13 +91,14 @@ ard_glad_download_ts <- function(x, name, dir, prefix, return_filename) {
     })
   )
 
-  cli::cli_alert_info("Downloading files for time period: {name}")
+  if (!quiet) cli::cli_alert_info("Downloading files for time period: {name}")
 
   down_df <- curl::multi_download(x,
     destfiles = out_files,
     userpwd = "glad:ardpas",
     resume = TRUE,
-    multiplex = TRUE
+    multiplex = TRUE,
+    progress = !quiet
   )
 
   check_status_codes(down_df)
@@ -139,124 +139,4 @@ check_status_codes <- function(x) {
       )
     )
   }
-}
-
-#' print method for ard_glad objects
-#' @rdname ard-glad-download
-#' @family GLAD ARD download
-#' @param x an ard_glad object
-#' @param ... not used
-#' @export
-print.ard_glad <- function(x, ...) {
-  cli::cli_h3(
-    cli::col_br_magenta("< ARD GLAD {cli::no(length(x))} SpatRaster{?s}. >")
-  )
-  pretty_p <- function(i) {
-    cli::cli_h1(paste0("[[{which(names(x) == i)}]] ", i))
-    y <- x[[i]]
-    print(y)
-  }
-  lapply(names(x), pretty_p)
-  invisible()
-}
-
-#' print method for ard_glad objects
-#' @rdname ard-glad-download
-#' @family GLAD ARD download
-#' @param x an ard_glad_src object
-#' @param ... not used
-#' @export
-print.ard_glad_src <- function(x, ...) {
-  cli::cli_h3(
-    cli::col_br_magenta("< {length(x)} ARD GLAD raster source{?s} >")
-  )
-  pretty_p <- function(i) {
-    cli::cli_h1(paste0("[[{which(names(x) == i)}]] ", i))
-    y <- terra::rast(x[[i]])
-    cli::cli_inform(paste(cli::col_br_blue("source      :"), x[[i]]))
-    cli::cli_inform(paste(
-      cli::col_br_blue("dimensions  :"),
-      paste(dim(y), collapse = ", "), "(nrow, ncol, nlyr)"
-    ))
-    cli::cli_inform(paste(
-      cli::col_br_blue("resolution  :"),
-      paste(round(terra::res(y), 7), collapse = ", "), "(x, y)"
-    ))
-    cli::cli_inform(paste(
-      cli::col_br_blue("extent      :"),
-      paste(round(terra::ext(y), 7), collapse = ", "),
-      "(xmin, xmax, ymin, ymax)"
-    ))
-    crsinfo <- terra::crs(y, describe = TRUE)
-    cli::cli_inform(paste(
-      cli::col_br_blue("coord. ref. :"),
-      ifelse(
-        terra::is.lonlat(y),
-        "lon/lat",
-        NULL
-      ),
-      ifelse(
-        is.na(crsinfo$name),
-        NULL,
-        crsinfo$name
-      ),
-      ifelse(
-        all(!is.na(c(crsinfo$authority, crsinfo$code))),
-        glue::glue("({crsinfo$authority}:{crsinfo$code})"),
-        NULL
-      )
-    ))
-    cli::cli_inform(paste(
-      cli::col_br_blue("band names   :"),
-      paste(names(y), collapse = ", ")
-    ))
-  }
-  lapply(names(x), pretty_p)
-  invisible()
-}
-
-#' plot method for ard_glad objects
-#' @rdname ard-glad-download
-#' @family GLAD ARD download
-#' @param x a ard_glad object
-#' @param ... not used
-#' @export
-plot.ard_glad <- function(x) {
-  mapply(
-    \(y, z) {
-      terra::plotRGB(y,
-        r = 3, g = 2, b = 1,
-        zlim = c(0, 5000),
-        zcol = TRUE,
-        stretch = "lin",
-        axes = TRUE,
-        mar = c(2, 2, 2, 1),
-        main = z
-      )
-    },
-    x,
-    names(x)
-  )
-  invisible()
-}
-
-#' plot method for ard_glad objects
-#' @rdname ard-glad-download
-#' @family GLAD ARD download
-#' @param x a ard_glad_src object
-#' @param ... not used
-#' @export
-plot.ard_glad_src <- function(x) {
-  plot(convert_src_to_glad(x))
-}
-
-#' helper to covert the ard_glad_src to ard_glad
-#' @param x an ard_glad_src object
-#' @return an ard_glad object
-#' @noRd
-#' @keywords internal
-convert_src_to_glad <- function(x) {
-  x <- lapply(x, terra::rast)
-  class(x) <- c("ard_glad", class(x))
-  return(x)
 }
